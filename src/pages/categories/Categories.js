465051f4ba96079a69dd955850f09c978a9d3c0f
Kerem Erguner <ergunerkerem@hotmail.com>
2022-09-03 23:19:44 +0300

    lottie animation added

--- src/pages/categories/Categories.js
+++ src/pages/categories/Categories.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { SafeAreaView, View, Text, FlatList } from 'react-native'
 import Config from 'react-native-config'
 import CategoriesCard from '../../components/CategoriesCard'
 import useFetch from '../../hooks/useFetch'
+import Lottie from 'lottie-react-native';
 
 const Categories = () => {
 
@@ -13,11 +14,11 @@ const Categories = () => {
 
 
     if (loading) {
-        return <Text>Yukeln bakim</Text>
+        return <Lottie source={require('../../assets/loading.json')} autoPlay loop />
     }
 
     if (error) {
-        return <Text>Hata Meydana Geldi</Text>
+        return <Lottie source={require('../../assets/error.json')} autoPlay loop />
     }
 
     return (
