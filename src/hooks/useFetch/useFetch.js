a90cb734b27cae01afa18809cb35ddd38abfb454
Kerem Erguner <ergunerkerem@hotmail.com>
2022-09-03 22:22:09 +0300

    Custom hook added

---
+++ src/hooks/useFetch/useFetch.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function useFetch(url) {
+
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+
+
+
+    const fetchData = async () => {
+
+        try {
+            const { data: responseData } = await axios.get(url)
+            setData(responseData)
+            setLoading(false)
+
+        } catch (err) {
+            setError(err.message)
+            setLoading(false)
+            console.log(error)
+        }
+    }
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    return { data, loading, error };
+
+}
+
+export default useFetch
--- src/pages/categories/Categories.js
+++ src/pages/categories/Categories.js
@@ -3,29 +3,21 @@ import React, { useState, useEffect } from 'react'
 import { SafeAreaView, View, Text, FlatList } from 'react-native'
 import Config from 'react-native-config'
 import CategoriesCard from '../../components/CategoriesCard'
+import useFetch from '../../hooks/useFetch'
 
 const Categories = () => {
 
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
-
-
-    useEffect(() => {
-        fetchData();
-    }, [])
-
-    const fetchData = async () => {
-        const { data: response } = await axios.get(Config.API_CATEGORIES)
-        setData(response)
-        setLoading(false)
-    }
+    const { data, loading, error } = useFetch(Config.API_CATEGORIES)
 
     const renderCategory = ({ item }) => <CategoriesCard category={item} />;
 
 
     if (loading) {
         return <Text>Yukeln bakim</Text>
+    }
+
+    if (error) {
+        return <Text>Hata Meydana Geldi</Text>
     }
 
     return (
